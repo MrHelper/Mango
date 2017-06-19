1cdaa29d0bf5db69cdcbe857978c1cf33aa8214f
Hung Nguyen <hung.np988@gmail.com>
2017-06-19 22:51:33 +0700

    edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function Menu(){
 		$menu = Menu::get();
         return view('admin.menu')->with(['Menu'=>$menu]);
